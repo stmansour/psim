871dfdafaddc2045d810876f8a96436f1de3854f
Steve Mansour <sman@stevemansour.com>
2023-02-17 20:47:31 -0800

    WIP

--- apps/dr/main.go
+++ apps/dr/main.go
@@ -106,167 +106,155 @@ func main() {
 	dtEnd := dtStop.AddDate(0, 0, 1)
 	fmt.Printf("Probs from %s to %s\n", dtStart.Format("Jan 2, 2006"), dtEnd.Format("Jan 2, 2006"))
 
+	// CSV column headings
 	fmt.Printf("t1,t2,t3,t4, dt1, dt2, dt4, dDRR, dERR, prediction, actual\n")
+
 	for dt := dtStart; dtEnd.After(dt); dt = dt.AddDate(0, 0, 1) {
-		fmt.Printf("%s\n", dt.Format("Jan 2, 2006"))
-		generateProbabilities(dt)
+		genProbs(dt)
 	}
 
+	// Now we need to compute the accuracy.  That is, when the prediction was "buy" how
+	// often was it right.
+	// TODO: how often was it correct when it said to hold?  This might be worth knowing
+	//----------------------------------------------------------------------------------
+	fmt.Printf("Sig1,Sig2,Sig3,Correct Predictions,Total Predictions, Correct Pct\n")
 	for i := core.DR.T1min; i <= core.DR.T1max; i++ {
 		for j := core.DR.T2min; j <= core.DR.T2max; j++ {
+			if i == j {
+				continue
+			}
 			for k := core.DR.T4min; k <= core.DR.T4max; k++ {
 				s := fmt.Sprintf("%d,%d,%d", i, j, k)
 				v := app.probMap[s]
-				fmt.Printf("%s : %d / %d  ->  %7.4f\n", s, v.correct, v.count, v.prob)
+				fmt.Printf("%s, %d, %d, %5.1f%%\n", s, v.correct, v.count, 100.0*v.prob)
 			}
 		}
 	}
 }
 
-// generateProbablilities is the routine used to generate the probabilities for
-// the DiscountRate Influencer.
+func genProbs(t3 time.Time) {
+	l := 0
+	for i := core.DR.T1min; i <= core.DR.T1max; i++ {
+		t1 := t3.AddDate(0, 0, i)
+		for j := core.DR.T2min; j <= core.DR.T2max; j++ {
+			if i == j {
+				continue
+			}
+			t2 := t3.AddDate(0, 0, j)
+			for k := core.DR.T4min; k <= core.DR.T4max; k++ {
+				t4 := t3.AddDate(0, 0, k)
+				l++
+				// fmt.Printf("%d:  %d,%d,%d:   t1: %s, t2: %s, t4: %s\n", l, i, j, k, t1.Format("Jan 2, 2006"), t2.Format("Jan 2, 2006"), t4.Format("Jan 2, 2006"))
+				computeDRProbability(t1, t2, t3, t4, i, j, k)
+
+			}
+		}
+	}
+}
+
+// computeDRProbability
+//
+// INPUTS
+//
+//			t1 - first date of DRR analysis
+//		    t2 - last date of DRR analysis
+//		    t3 - check date, that is the date of buy or hold
+//		    t4 - sell date if the prediction is to buy
+//		    dt1, dt2, dt4 - these 3 numbers form the unique signature of a
+//		         DiscountRate Influencer.  Their meanings are:
+//	             * # days prior to check date to start analysis
+//	             * # days prior to check date to stop analysis
+//	             * # days after check date to sell (if the prediction is to buy)
+//
 // --------------------------------------------------------------------------------
-func generateProbabilities(t3 time.Time) {
-
-	//--------------------------------------------------------
-	// Start the simulation on core.DR.T1min days after t3
-	//--------------------------------------------------------
-	dtT1min := t3.AddDate(0, 0, core.DR.T1min)   // t1 starts here
-	dtT1max := t3.AddDate(0, 0, 1+core.DR.T1max) // t1 goes up to but does not include this date
-
-	dtT2min := t3.AddDate(0, 0, core.DR.T2min)   // t2 starts here
-	dtT2max := t3.AddDate(0, 0, 1+core.DR.T2max) // t2 goes up to but does not include this date
-
-	dtT4min := t3.AddDate(0, 0, core.DR.T4min)   // t2 starts here
-	dtT4max := t3.AddDate(0, 0, 1+core.DR.T4max) // t2 goes up to but does not include this date
-
-	t1a := core.DR.T1min // range for t1 relative to t3
-	// t1b := core.DR.T1max
-
-	// t2a := core.DR.T2min // range for t2 relative to t3
-	// t2b := core.DR.T2max
-
-	// t4a := core.DR.T4min // range for t4 relative to t3
-	// t4b := core.DR.T4max
-
-	dt2 := 0 // define the deltas for each of the moving dates
-	dt4 := 0
-	dt1 := t1a
-
-	// fmt.Printf("t3 range:  [ %s , %s )\n", dtT1min.Format("Jan 2, 2006"), dtT1max.Format("Jan 2, 2006"))
-
-	n := 0
-	//------------------------------------------------------------------------------
-	// t1 defines how far back to examine Exchange Rates prior to t3
-	//------------------------------------------------------------------------------
-	for t1 := dtT1min; t1.Before(dtT1max); t1 = t1.AddDate(0, 0, 1) {
-		dt2 = core.DR.T2min // restart each time through
-		//------------------------------------------------------------------------------
-		// t2 defines how far back to examine Exchange Rates prior to t3
-		//------------------------------------------------------------------------------
-		for t2 := dtT2min; t2.Before(dtT2max); t2 = t2.AddDate(0, 0, 1) {
-			if t2.After(t1.AddDate(0, 0, 1)) {
-				dt4 = core.DR.T4min // restart each time throu
-				for t4 := dtT4min; t4.Before(dtT4max); t4 = t4.AddDate(0, 0, 1) {
-					//---------------------------------------------------------------------------
-					// Determine dDRR = (DiscountRateRatio at t1) - (DiscountRateRatio at t2)
-					//---------------------------------------------------------------------------
-					rec1 := data.DRFindRecord(t1)
-					if rec1 == nil {
-						fmt.Printf("ExchangeRate Record for %s not found.\n", t1.Format("1/2/2006"))
-						os.Exit(1)
-					}
-					rec2 := data.DRFindRecord(t2)
-					if rec2 == nil {
-						fmt.Printf("ExchangeRate Record for %s not found.\n", t2.Format("1/2/2006"))
-						os.Exit(1)
-					}
-					dDRR := rec1.USJPDRRatio - rec2.USJPDRRatio
-
-					//-------------------------------------------------------------------------------
-					// Prediction formula (based on the change in DiscountRateRatios):
-					//     dDRR > 0:   buy on t3, sell on t4
-					//     dDRR <= 0:  take no action
-					//-------------------------------------------------------------------------------
-					prediction := "hold"
-					if dDRR > 0 {
-						prediction = "buy"
-					}
-
-					//-------------------------------------------------------------------------------
-					// Determine deltaER (dER) =
-					//         (ExchangeRateRatio at t3) - (ExchangeRateRatio at t4)
-					//-------------------------------------------------------------------------------
-					er1 := data.ERFindRecord(t3)
-					if er1 == nil {
-						fmt.Printf("ExchangeRate Record for %s not found.\n", t3.Format("1/2/2006"))
-						os.Exit(1)
-					}
-					er2 := data.ERFindRecord(t4)
-					if er2 == nil {
-						fmt.Printf("ExchangeRate Record for %s not found.\n", t4.Format("1/2/2006"))
-						os.Exit(1)
-					}
-					dER := er1.Close - er2.Close
-
-					//-------------------------------------------------------------------------------
-					// Check to see if the prediction is correct. If dDRR > 0 AND dER > 0 then
-					// then the prediction was correct.
-					//-------------------------------------------------------------------------------
-					predictionResult := false
-					if dER > 0 && dDRR > 0 {
-						predictionResult = true
-					}
-
-					//-------------------------------------------------------------------------------
-					// Record the correctness.  A unique DiscountRateInfluencer type is defined by its
-					// t1, t2, and t4 values.  Record the correctness (predictionResult) for each
-					// DiscountInfluencer type
-					//-------------------------------------------------------------------------------
-					addToProbabilities(dt1, dt2, dt4, predictionResult)
-
-					//-------------------------------------------------------------------------------
-					// Print out for manual checking...
-					//-------------------------------------------------------------------------------
-					fmt.Printf("%s,%s,%s,%s,%d,%d,%d,%6.2f,%6.2f,%s,%t\n",
-						t1.Format("01/02/2006"), t2.Format("01/02/2006"),
-						t3.Format("01/02/2006"), t4.Format("01/02/2006"),
-						dt1, dt2, dt4,
-						dDRR, dER,
-						prediction,
-						predictionResult,
-					)
-					dt4++
-					n++
-				}
-			}
-			dt2++
-		}
-		dt1++
+func computeDRProbability(t1, t2, t3, t4 time.Time, dt1, dt2, dt4 int) {
+	//---------------------------------------------------------------------------
+	// Determine dDRR = (DiscountRateRatio at t1) - (DiscountRateRatio at t2)
+	//---------------------------------------------------------------------------
+	rec1 := data.DRFindRecord(t1)
+	if rec1 == nil {
+		fmt.Printf("ExchangeRate Record for %s not found.\n", t1.Format("1/2/2006"))
+		os.Exit(1)
 	}
-	fmt.Printf("total possibilities: %d\n", n)
+	rec2 := data.DRFindRecord(t2)
+	if rec2 == nil {
+		fmt.Printf("ExchangeRate Record for %s not found.\n", t2.Format("1/2/2006"))
+		os.Exit(1)
+	}
+	dDRR := rec1.USJPDRRatio - rec2.USJPDRRatio
+
+	//-------------------------------------------------------------------------------
+	// Prediction formula (based on the change in DiscountRateRatios):
+	//     dDRR > 0:   buy on t3, sell on t4
+	//     dDRR <= 0:  take no action
+	//-------------------------------------------------------------------------------
+	prediction := "hold"
+	if dDRR > 0 {
+		prediction = "buy"
+	}
+
+	//-------------------------------------------------------------------------------
+	// Determine deltaER (dER) =
+	//         (ExchangeRateRatio at t3) - (ExchangeRateRatio at t4)
+	//-------------------------------------------------------------------------------
+	er1 := data.ERFindRecord(t3)
+	if er1 == nil {
+		fmt.Printf("ExchangeRate Record for %s not found.\n", t3.Format("1/2/2006"))
+		os.Exit(1)
+	}
+	er2 := data.ERFindRecord(t4)
+	if er2 == nil {
+		fmt.Printf("ExchangeRate Record for %s not found.\n", t4.Format("1/2/2006"))
+		os.Exit(1)
+	}
+	dER := er1.Close - er2.Close
+
+	//-------------------------------------------------------------------------------
+	// Check to see if the prediction is correct. If dDRR > 0 AND dER > 0 then
+	// then the prediction was correct.
+	//-------------------------------------------------------------------------------
+	predictionResult := false
+	if dER > 0 && dDRR > 0 {
+		predictionResult = true
+	}
+
+	//-------------------------------------------------------------------------------
+	// Record the correctness.  A unique DiscountRateInfluencer type is defined by its
+	// t1, t2, and t4 values.  Record the correctness (predictionResult) for each
+	// DiscountInfluencer type
+	//-------------------------------------------------------------------------------
+	addToProbabilities(dt1, dt2, dt4, prediction, predictionResult)
+
+	//-------------------------------------------------------------------------------
+	// Print out for manual checking...
+	//-------------------------------------------------------------------------------
+	fmt.Printf("%s,%s,%s,%s,%d,%d,%d,%6.2f,%6.2f,%s,%t\n",
+		t1.Format("01/02/2006"), t2.Format("01/02/2006"),
+		t3.Format("01/02/2006"), t4.Format("01/02/2006"),
+		dt1, dt2, dt4,
+		dDRR, dER,
+		prediction,
+		predictionResult,
+	)
+
 }
 
-func addToProbabilities(i, j, k int, correct bool) {
+func addToProbabilities(i, j, k int, prediction string, predWasCorrect bool) {
 	s := fmt.Sprintf("%d,%d,%d", i, j, k)
 	v, ok := app.probMap[s]
-	if ok {
-		v.count++
-		if correct {
-			v.correct++
-		}
-		v.prob = float64(v.correct) / float64(v.count)
-		app.probMap[s] = v
-	} else {
-		v := probInfo{
-			count:   1,
+	if !ok {
+		v = probInfo{
+			count:   0,
 			correct: 0,
 			prob:    0.0,
 		}
-		if correct {
-			v.correct = 1
+	}
+	if prediction == "buy" {
+		v.count++
+		if predWasCorrect {
+			v.correct++
 		}
 		v.prob = float64(v.correct) / float64(v.count)
-		app.probMap[s] = v
 	}
+	app.probMap[s] = v
 }
