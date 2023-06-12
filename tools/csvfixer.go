fca3116d58d81b9f444e4d9f21ab860582c2305b
Steve Mansour <sman@stevemansour.com>
2023-06-12 15:06:15 -0700

    WIP

--- tools/csvfixer.go
+++ tools/csvfixer.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 	"os"
 	"time"
+
+	"github.com/stmansour/psim/util"
 )
 
 var app struct {
@@ -49,27 +51,27 @@ func main() {
 		fmt.Printf("*ERROR* the first column of the csv file must be Date\n")
 		os.Exit(1)
 	}
-	d1, err = time.Parse("1/02/2006", records[1][0])
+	d1, err = util.StringToDate(records[1][0])
 	if err != nil {
 		fmt.Println("Error parsing date:", err)
 		os.Exit(1)
 	}
-	d2, err = time.Parse("1/02/2006", records[len(records)-1][0])
+	d2, err = util.StringToDate(records[len(records)-1][0])
 	if err != nil {
 		fmt.Println("Error parsing date:", err)
 		os.Exit(1)
 	}
 	d2 = d2.AddDate(0, 0, 1)
 
-	fmt.Printf("Start date: %s\n", d1.Format("01/02/2006"))
-	fmt.Printf("Stop date: %s\n", d2.Format("01/02/2006"))
+	// fmt.Printf("Start date: %s\n", d1.Format("01/02/2006"))
+	// fmt.Printf("Stop date: %s\n", d3.Format("01/02/2006"))
 
 	//---------------------------------------------------------------
 	// Start by printing out the column headers...
 	//---------------------------------------------------------------
 	for i := 0; i < len(cols); i++ {
 		s := cols[i]
-		if i < len(cols)-2 {
+		if i < len(cols)-1 {
 			s += ","
 		}
 		fmt.Printf("%s", s)
@@ -83,28 +85,33 @@ func main() {
 	// first value we have is for March 1, then we use the March 1
 	// value for every day of March.
 	//---------------------------------------------------------------
-	lastRow := 1
-	i := 1
-	for dt := d1; dt.Before(d2); dt = dt.Add(24 * time.Hour) {
-		// compare dt to the date in record[i]
-		d1, err = time.Parse("1/02/2006", records[i][0])
+	printFromThisRecord := 1
+	i := 0 // how many rows of the input records have we processed
+	for dtLoop := d1; dtLoop.Before(d2); dtLoop = dtLoop.Add(24 * time.Hour) {
+		fmt.Printf("%s", dtLoop.Format("1/02/2006")) // This is the date we're on now
+
+		// what's the next date in the records...
+		if len(records) > i+1 {
+			i++
+		}
+		dtNextRecord, err := util.StringToDate(records[i][0])
 		if err != nil {
 			fmt.Println("Error parsing date:", err)
 			os.Exit(1)
 		}
-		fmt.Printf("%s", dt.Format("1/02/2006"))
-		idx := i // assume this is a new row
-		if dt.After(d1) {
-			idx = lastRow // adjust, fill using value from previous record
-		} else {
-			lastRow = i // mark that we are on a new row
+
+		// has our loop date (dtLoop) reached the date of the next record (dtNextRecord)?
+		if dtLoop.Equal(dtNextRecord) || dtLoop.After(dtNextRecord) { // has the current loop date reached or passed that of the record?
+			printFromThisRecord = i // we're now going to be printing THIS record until the loop date reaches or passes this row's date
 		}
+
 		//-------------------------------------
 		// print all columns after the date...
 		//-------------------------------------
-		for j := 1; j < len(records[idx]); j++ {
-			fmt.Printf(",%s", records[lastRow][j])
+		for j := 1; j < len(records[printFromThisRecord]); j++ {
+			fmt.Printf(",%s", records[printFromThisRecord][j])
 		}
 		fmt.Printf("\n")
 	}
+
 }
