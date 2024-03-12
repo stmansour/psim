784fcc88897f65b98c74d171a95d7b86ead0193d
Steve Mansour <sman@stevemansour.com>
2024-03-12 07:44:06 +0000

    fix issues in csvdb locating

--- newdata/csvdb.go
+++ newdata/csvdb.go
@@ -39,9 +39,16 @@ import (
 // LoadCsvDB - Read in the data from the CSV file
 // -----------------------------------------------------------------------------
 func (d *DatabaseCSV) LoadCsvDB() error {
-	fname := PLATODB // this is the default: data/platodb.csv
+	fname := "" // this is the default: data/platodb.csv
 	if len(d.DBFname) > 0 {
 		fname = d.DBFname
+	} else {
+	    dir, err := util.GetExecutableDir()
+	    if err != nil {
+		    fmt.Println("Error getting executable directory:", err)
+		    os.Exit(1)
+	    }
+	    fname = dir + "/" + PLATODB
 	}
 
 	file, err := os.Open(fname)
