c7e2042a662e6b3856740a117a823ba8a6d1ebd7
Steve Mansour <sman@stevemansour.com>
2024-07-07 17:27:33 -0700

    updated simtalk to deal with multple simulator processes on the same computer

--- apps/simtalk/main.go
+++ apps/simtalk/main.go
@@ -5,10 +5,12 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"net"
 	"net/http"
 	"os"
 	"strconv"
 	"strings"
+	"time"
 )
 
 // Command is a struct that holds the command name and its description
@@ -40,8 +42,46 @@ type StopResponse struct {
 	Message string
 }
 
+var app struct {
+	ports []int // list of ports between 8090 and 8100 with listeners
+	pidx  int   // index into the list of ports
+}
+
+// checkPort tries to establish a TCP connection to the given port and returns true if successful
+func checkPort(port int) bool {
+	address := fmt.Sprintf("localhost:%d", port)
+	conn, err := net.DialTimeout("tcp", address, 1*time.Second)
+	if err != nil {
+		return false
+	}
+	conn.Close()
+	return true
+}
+
+// scanPorts scans the specified range of ports and returns a list of ports with listeners
+func scanPorts(startPort, endPort int) []int {
+	var openPorts []int
+	for port := startPort; port <= endPort; port++ {
+		if checkPort(port) {
+			openPorts = append(openPorts, port)
+		}
+	}
+	return openPorts
+}
+
 func main() {
-	port := 8090
+	var port int
+	//---------------------------------------------------
+	// SEE IF THERE ARE ANY SIMULATOR PROCESSES RUNNING
+	//---------------------------------------------------
+	app.ports = scanPorts(8090, 8100)
+	if len(app.ports) == 0 {
+		app.pidx = -1
+	}
+
+	//---------------------------------------------------
+	// SELECT THE ONE THE USER INDICATED IF POSSIBLE...
+	//---------------------------------------------------
 	var err error
 	if len(os.Args) > 1 {
 		port, err = strconv.Atoi(os.Args[1])
@@ -49,52 +89,152 @@ func main() {
 			fmt.Printf("Invalid port number: %s\n", os.Args[1])
 			os.Exit(1)
 		}
+		for i := 0; i < len(app.ports); i++ {
+			if app.ports[i] == port {
+				app.pidx = i
+				break
+			}
+		}
+		if app.pidx != port {
+			fmt.Printf("No simulator processes running on port %d.\n", port)
+			fmt.Printf("Currently selected simulator process is listening on port %d.\n", app.ports[app.pidx])
+		}
 	}
 
-	baseURL := fmt.Sprintf("http://localhost:%d", port)
-
 	commands := []Command{
+		{"help", "List the available commands."},
+		{"next", "Select the next simulator in the list."},
+		{"port <portnumber>", "Switch to port <portnumber>."},
+		{"ports", "List the available ports."},
+		{"prev", "Select the previous simulator in the list."},
+		{"rescan", "Rescan the list of available ports."},
 		{"status", "Get the current status of the simulator."},
 		{"stopsim", "Tell the simulator to stop after completing the current generation."},
 	}
 
-	resp, err := http.Get(baseURL + "/status")
-	if err != nil {
-		fmt.Printf("Failed to connect to the simulator on port %d. Ensure it is running.\n", port)
-		os.Exit(1)
+	if len(app.ports) > 1 {
+		fmt.Printf("Found %d simulators running on ports these ports:\n", len(app.ports))
+		for i := 0; i < len(app.ports); i++ {
+			fmt.Printf("  %d\n", app.ports[i])
+		}
 	}
-	resp.Body.Close()
-
-	fmt.Println("Connected to the simulator. Enter 'help' for a list of commands.")
+	if len(app.ports) == 0 {
+		noSimulatorsMessage()
+	} else {
+		fmt.Printf("Connected to the simulator on port %d\n", port)
+		fmt.Printf("Use 'port', or 'next', or 'prev' to select a different simulator.\n")
+	}
+	fmt.Println("Enter 'help' for a list of commands.")
 
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
-		fmt.Printf("@simulator:%d > ", port)
+		prompt := "simtalk"
+		if app.pidx >= 0 {
+			prompt += fmt.Sprintf(" simulator@%d", app.ports[app.pidx])
+		}
+		fmt.Printf("%s> ", prompt)
 		scanned := scanner.Scan()
 		if !scanned {
 			break
 		}
 
 		text := scanner.Text()
+		args := strings.Split(text, " ")
 		trimmedText := strings.TrimSpace(text)
+		switch args[0] {
+		case "port":
+			if app.pidx < 0 {
+				noSimulatorsMessage()
+				continue
+			}
+			if len(args) != 2 {
+				noSimulatorsMessage()
+				continue
+			}
+			port, err = strconv.Atoi(args[1])
+			if err != nil {
+				fmt.Println("Invalid port number")
+				continue
+			}
+			fmt.Printf("Switching to simulator on port %d\n", port)
+			found := false
+			for i := 0; i < len(app.ports); i++ {
+				if app.ports[i] == port {
+					app.pidx = i
+					found = true
+					break
+				}
+			}
+			if !found {
+				fmt.Printf("No simulator processes running on port %d.\n", port)
+				fmt.Printf("Currently selected simulator process is listening on port %d.\n", app.ports[app.pidx])
+				continue
+			}
+			continue
 
-		if trimmedText == "help" {
+		case "ports":
+			if len(app.ports) == 0 {
+				noSimulatorsMessage()
+				continue
+			}
+			fmt.Printf("Available ports:\n")
+			for i := 0; i < len(app.ports); i++ {
+				fmt.Printf("  %d\n", app.ports[i])
+			}
+			continue
+
+		case "next":
+			if app.pidx < 0 {
+				noSimulatorsMessage()
+				continue
+			}
+			app.pidx = (app.pidx + 1) % len(app.ports)
+			continue
+
+		case "prev":
+			if app.pidx < 0 {
+				noSimulatorsMessage()
+				continue
+			}
+			app.pidx = (app.pidx - 1 + len(app.ports)) % len(app.ports)
+			continue
+
+		case "rescan":
+			rescan()
+			continue
+
+		case "help":
 			fmt.Println("Available commands:")
 			for _, cmd := range commands {
 				fmt.Printf("- %s : %s\n", cmd.Name, cmd.Description)
 			}
-			continue
-		} else if trimmedText == "quit" || trimmedText == "exit" {
-			break
+
+		case "quit":
+			fallthrough
+		case "exit":
+			os.Exit(0)
 		}
 
+		//-------------------------------------
+		// SEND THE COMMAND TO SIMULATOR
+		//-------------------------------------
+		if app.pidx < 0 {
+			noSimulatorsMessage()
+			continue
+		}
+		baseURL := fmt.Sprintf("http://localhost:%d", app.ports[app.pidx])
 		response, err := sendCommand(baseURL, trimmedText)
 		if err != nil {
+			if strings.Contains(err.Error(), "connect: connection refused") {
+				fmt.Printf("A simulator is no longer running on port %d. Rescanning...\n", app.ports[app.pidx])
+				rescan()
+				continue
+			}
 			fmt.Printf("Error sending command: %v\n", err)
 			continue
 		}
-
 		fmt.Println(response)
+
 	}
 }
 
@@ -163,4 +303,25 @@ func formatSimulatorStatus(status SimulatorStatus) string {
 		status.EstimatedCompletion,
 		status.SID,
 	)
+}
+
+func rescan() {
+	app.pidx = -1
+	app.ports = scanPorts(8090, 8100)
+	if len(app.ports) == 0 {
+		fmt.Printf("No simulators appear to be running on this computer\n")
+		fmt.Printf("Use 'rescan' to rescan the list of available ports\n")
+		return
+	}
+	app.pidx = 0
+	fmt.Printf("Fount %d simulators running on ports these ports:\n", len(app.ports))
+	for i := 0; i < len(app.ports); i++ {
+		fmt.Printf("  %d\n", app.ports[i])
+	}
+	fmt.Printf("Now talking to simulator on port %d\n", app.ports[app.pidx])
+}
+
+func noSimulatorsMessage() {
+	fmt.Printf("No simulators appear to be running on this computer\n")
+	fmt.Printf("Use 'rescan' to rescan the list of available ports\n")
 }
