998c12d281cf43ed0fb16632b761f7ec82f8bb5b
Steve Mansour <sman@stevemansour.com>
2024-08-07 12:06:59 -0700

    FIX: we can now update the full csv datafile set, GDELT sync completed

--- tools/bash/dogsync.sh
+++ tools/bash/dogsync.sh
@@ -61,8 +61,8 @@ rm -rf data
 "${SQLTOCSV}" ; pushd data ; "${TSF}" platodb.csv ; mv platodb-filled.csv platodb.csv ; popd
 log "Posting new CSV fileset to ${HTTPDOCPATH}/csv/"
 tar cvf USDJPYdata.tar data ; gzip -f USDJPYdata.tar ; cp USDJPYdata.tar.gz ${HTTPDOCPATH}/csv/
-cp data/platodb.csv /usr/local/plato/bin/data/
-cp data/platodb.csv /var/www/html/viewer/data/
+tar xzvf USDJPYdata.tar.gz -C /usr/local/plato/bin
+tar xzvf USDJPYdata.tar.gz -C /var/www/html/viewer
 
 #------------------------------------------------------------------------------
 # Now create the status from the results of yesterday's gdelt update...
